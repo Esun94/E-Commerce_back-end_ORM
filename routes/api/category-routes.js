26e5aff83e3bf226676e6315ee701ae33e56764f
Eric Sun <ericmsun@gmail.com>
2021-09-12 13:14:58 -0400

    saving changes to Models files

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,7 +3,13 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const categories = Category.findAll();
+    res.json(categories)
+  } catch (error) {
+    res.json(error)
+  }
   // find all categories
   // be sure to include its associated Products
 });
