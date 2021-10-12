f5e8907f13661a98b47f8a6474f0e0768bcc9ebe
Eric Sun <ericmsun@gmail.com>
2021-10-11 21:51:24 -0400

    saving Create New Tag and Update Tag by ID Route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
-    const tagData = await Tag.findAll ({
+    const tagData = await Tag.findAll({
       include: [{ model: Product, ProductTag }],
     });
     res.status(200).json(tagData);
@@ -31,10 +31,29 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const newTag = await Tag.create(req.body);
+    res.status(200).json(newTag)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updatedTag = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (updatedTag[0])
+      res.status(200).json(updatedTag[1]);
+    else
+      res.status(404).json({ message: "No Tag updated." });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
